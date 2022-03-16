403be0f47aa428f72d4d701f22eb8fe6a3c6b727
Mujahedyousef <mujahedyousef97@gmail.com>
2022-03-16 22:30:23 +0300

    update

--- Auth/middlewares/basicAuth.js
+++ Auth/middlewares/basicAuth.js
@@ -1,10 +1,12 @@
 "use strict"
+
 require('dotenv').config()
-const base64 = require('base-64')
-const { user } = require('../models/index.js')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const SECRET = process.env.SECRET
+const base64 = require('base-64');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+const { user } = require('../models/index');
+const bcrypt=require('bcrypt')
+
 
 const basicAuth = async (req, res, next) => {
     let basicAuthText = req.headers.authorization;
@@ -13,21 +15,16 @@ const basicAuth = async (req, res, next) => {
             let basicHeardersParts = basicAuthText.split(' ');
             let encoded = basicHeardersParts.pop();
             let decode = base64.decode(encoded);
-            console.log(decode);
             let [username, password] = decode.split(":")
             const User = await user.findOne({ where: { username: username } });
-            // console.log(User.password);
-            // console.log(password);
             const valid = await bcrypt.compare(password, User.password);
-            console.log(valid);
             if (valid) {
-                req.User = User;
-                let newToken = jwt.sign({ username: User.username }, SECRET, { expiresIn: 900000 })
+                req.User=User
+                let newToken = jwt.sign({ username: User.username }, SECRET,{expiresIn : 900000})
                 User.token = newToken;
                 res.status(200).json(User)
                 next()
             } else {
-                console.error;
                 res.status(403).send('invalid sign in Password')
             }
         }
@@ -37,4 +34,5 @@ const basicAuth = async (req, res, next) => {
         res.status(403).send('invalid sign in Username')
     }
 }
+
 module.exports = basicAuth;
