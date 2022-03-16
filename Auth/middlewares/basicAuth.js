1be2003f318e24e583d9ea2cbf00c825e207d2cd
Mujahed Abuarqob <97676558+Mujahedyousef@users.noreply.github.com>
2022-03-17 00:32:36 +0300

    Update basicAuth.js

--- Auth/middlewares/basicAuth.js
+++ Auth/middlewares/basicAuth.js
@@ -3,7 +3,7 @@
 require('dotenv').config()
 const base64 = require('base-64');
 const jwt = require('jsonwebtoken');
-const SECRET = process.env.SECRET || "mujahedyousef"
+const SECRET = process.env.SECRET || 
 const { user } = require('../models/index');
 const bcrypt = require('bcrypt')
 
